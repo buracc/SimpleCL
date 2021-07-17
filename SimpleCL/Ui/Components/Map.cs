c3a1d3a5068fc96f8584120e39071c438c459b55
Burak Inan <burakinan1@hotmail.com>
2021-07-17 20:15:52 +0200

    Include custom components in compile

--- SimpleCL/Ui/Components/Map.cs
+++ SimpleCL/Ui/Components/Map.cs
@@ -3,7 +3,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using SimpleCL.Interaction.Pathing;
 using SimpleCL.Interaction.Providers;
